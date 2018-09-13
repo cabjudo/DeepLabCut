7ef7d6aa6fcdaccab7ccfe1cc292300ebe10c8de
Christine <allec@seas.upenn.edu>
2018-09-13 09:14:40 -0400

    Modify to reflect changes due to training

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# go to folder
+cd Generating_a_Training_Set
+
+# run Step 2
+# python Step2_ConvertingLabels2DataFrame.py
+# python Step3_CheckLabels.py
+# python Step4_GenerateTrainingFileFromLabelledData.py
+
+cd ../pose-tensorflow/models/pretrained
+./download.sh
+
+cd ../..
+# cp -r YOURexperimentNameTheDate-trainset95shuffle1 ../pose-tensorflow/models/
+# cp -r UnaugmentedDataSet_YOURexperimentNameTheDate ../pose-tensorflow/models/
+
+cd ../pose-tensorflow/models/reachingJan30-trainset95shuffle1/train
+python ../../../train.py
