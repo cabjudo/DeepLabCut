c5823903aa9a9c2b81243d8caa5df3e4efbde083
Christine <allec@seas.upenn.edu>
2018-09-18 13:25:08 -0400

    Modify run script for train only

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # go to folder
-cd Generating_a_Training_Set
+# cd Generating_a_Training_Set
 
 # run Step 2
 # python Step2_ConvertingLabels2DataFrame.py
 # python Step3_CheckLabels.py
 # python Step4_GenerateTrainingFileFromLabelledData.py
 
-cd ../pose-tensorflow/models/pretrained
-./download.sh
+# cd ../pose-tensorflow/models/pretrained
+# ./download.sh
 
-cd ../..
+# cd ../..
 # cp -r YOURexperimentNameTheDate-trainset95shuffle1 ../pose-tensorflow/models/
 # cp -r UnaugmentedDataSet_YOURexperimentNameTheDate ../pose-tensorflow/models/
 
-cd ../pose-tensorflow/models/reachingJan30-trainset95shuffle1/train
+cd pose-tensorflow/models/reachingJan30-trainset95shuffle1/train
 python3 ../../../train.py
